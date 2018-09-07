3c6b7a0c1f4336dd96ba75df0c1f1e24023419ba
aryyya <arya@live.ca>
2018-09-06 17:50:47 -0700

    Serve a basic index.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const path = require('path')
 
 const server = express()
 const port = process.env.PORT || 3001
 
 server.get('/', (req, res) => {
-  console.log('Hello, world!')
+  res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
 server.listen(port, () => {
