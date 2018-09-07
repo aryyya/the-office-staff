5fc02df45d6e71c9d8f7a4d74b69655024f6bc9d
aryyya <arya@live.ca>
2018-09-06 18:35:02 -0700

    Add basic data and api endpoints.

---
+++ src/data.js
@@ -0,0 +1,219 @@
+const staff = {
+  'michael-scott': {
+    name: 'Michael Scott',
+    aliases: [
+      'Michael Scarn',
+      'Ping',
+      'Mykonos'
+    ],
+    gender: 'm'
+  },
+  'dwight-schrute': {
+    name: 'Dwight Schrute',
+    aliases: [
+      'D',
+      'Dragon',
+      'D-Money'
+    ],
+    gender: 'm'
+  },
+  'jim-halpert': {
+    name: 'Jim Halpert',
+    aliases: [
+      'Big Tuna',
+      'Big Haircut',
+      'Fat Halpert'
+    ],
+    gender: 'm'
+  },
+  'pam-beesly': {
+    name: 'Pam Beesly',
+    aliases: [
+      'Pam Halpert',
+      'Pama-lama-ding-dong',
+      'Pam-Pam'
+    ],
+    gender: 'f'
+  },
+  'andy-bernard': {
+    name: 'Andy Bernard',
+    aliases: [
+      'Nard Dog',
+      'Bro Nard',
+      'Boner Champ'
+    ],
+    gender: 'm'
+  },
+  'kevin-malone': {
+    name: 'Kevin Malone',
+    aliases: [
+      'Kev',
+      'Chumbo',
+      'Kool Aid Man'
+    ],
+    gender: 'm'
+  },
+  'angela-martin': {
+    name: 'Angela Martin',
+    aliases: [
+      'Monkey',
+      'Kitchen Witch'
+    ],
+    gender: 'f'
+  },
+  'erin-hannon': {
+    name: 'Erin Hannon',
+    aliases: [
+      'Kelly Hannon'
+    ],
+    gender: 'f'
+  },
+  'oscar-martinez': {
+    name: 'Oscar Martinez',
+    aliases: [
+      'C-Span',
+      'Actually'
+    ],
+    gender: 'm'
+  },
+  'darryl-philbin': {
+    name: 'Darryl Philbin',
+    aliases: [
+      'none'
+    ],
+    gender: 'm'
+  },
+  'ryan-howard': {
+    name: 'Ryan Howard',
+    aliases: [
+      'Temp',
+      'Fire Guy',
+      'Bryan'
+    ],
+    gender: 'm'
+  },
+  'phyllis-lapin': {
+    name: 'Phyllis Lapin',
+    aliases: [
+      'Phyllis Vance'
+    ],
+    gender: 'f'
+  },
+  'kelly-kapoor': {
+    name: 'Kelly Kapoor',
+    aliases: [
+      'none'
+    ],
+    gender: 'f'
+  },
+  'stanley-hudson': {
+    name: 'Stanley Hudson',
+    aliases: [
+      'Stan',
+      'Stanley the Manly'
+    ],
+    gender: 'm'
+  },
+  'meridith-palmer': {
+    name: 'Meridith Palmer',
+    aliases: [
+      'none'
+    ],
+    gender: 'f'
+  },
+  'creed-bratton': {
+    name: 'Creed Bratton',
+    aliases: [
+      'Old Man',
+      'Papa Smurf'
+    ],
+    gender: 'm'
+  },
+  'toby-flenderson': {
+    name: 'Toby Flenderson',
+    aliases: [
+      'none'
+    ],
+    gender: 'm'
+  }
+}
+
+const quotes = {
+  'michael-scott': [
+    ``
+  ],
+  'dwight-schrute': [
+    ``
+  ],
+  'jim-halpert': [
+    ``
+  ],
+  'pam-beesly': [
+    ``
+  ],
+  'andy-bernard': [
+    ``
+  ],
+  'kevin-malone': [
+    ``
+  ],
+  'angela-martin': [
+    ``
+  ],
+  'erin-hannon': [
+    ``
+  ],
+  'oscar-martinez': [
+    ``
+  ],
+  'darryl-philbin': [
+    ``
+  ],
+  'ryan-howard': [
+    ``
+  ],
+  'phyllis-lapin': [
+    ``
+  ],
+  'kelly-kapoor': [
+    ``
+  ],
+  'stanley-hudson': [
+    ``
+  ],
+  'meridith-palmer': [
+    ``
+  ],
+  'creed-bratton': [
+    ``
+  ],
+  'toby-flenderson': [
+    ``
+  ]
+}
+
+const images = {
+  'michael-scott': '',
+  'dwight-schrute': '',
+  'jim-halpert': '',
+  'pam-beesly': '',
+  'andy-bernard': '',
+  'kevin-malone': '',
+  'angela-martin': '',
+  'erin-hannon': '',
+  'oscar-martinez': '',
+  'darryl-philbin': '',
+  'ryan-howard': '',
+  'phyllis-lapin': '',
+  'kelly-kapoor': '',
+  'stanley-hudson': '',
+  'meridith-palmer': '',
+  'creed-bratton': '',
+  'toby-flenderson': ''
+}
+
+module.exports = {
+  staff,
+  quotes,
+  images
+}
--- src/server.js
+++ src/server.js
@@ -1,11 +1,54 @@
 const express = require('express')
 const path = require('path')
+const { staff, quotes, images } = require('./data')
 
 const server = express()
 const port = process.env.PORT || 3001
 
-server.get('/', (req, res) => {
+server.get('/', (_, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+server.get('/api/staff', (_, res) => {
+  const people = Object.keys(staff)
+  res.json(people)
+})
+
+server.get('/api/staff/:name', (req, res) => {
+  const { name } = req.params
+  const person = staff[name]
+  
+  if (person) {
+    res.json(person)
+  } else {
+    res.json({ error: `name "${name}" does not exist` })
+  }
+})
+
+server.get('/api/quote/:name', (req, res) => {
+  const { name } = req.params
+  const quote = quotes[name]
+
+  if (quote) {
+    res.json(quote)
+  } else {
+    res.json({ error: `name "${name}" does not exist` })
+  }
+})
+
+server.get('/api/image/:name', (req, res) => {
+  const { name } = req.params
+  const image = images[name]
+
+  if (image) {
+    res.json(image)
+  } else {
+    res.json({ error: `name "${name}" does not exist` })
+  }
+})
+
+server.get('*', (_, res) => {
+  res.json({ error: `no api endpoint exists here` })
 })
 
 server.listen(port, () => {
